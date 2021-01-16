c4a29692aeea66b725d9396aa0065c9e0cec296f
yashith <yashith98@gmail.com>
2021-01-16 20:04:10 +0530

    Modified BCL backlog no back end changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Auth from './Container/Auth'
+import IssueBacklogBCL from './Components/BCL/IssueBacklog/IssueBacklogBCL' //remove after done
 
 function App() {
   return (
     <div className="App">
       <Auth />
+      {/* <IssueBacklogBCL/> */}
     </div>
   );
 };
--- frontend/src/Components/BCL/IssueBacklog/IssueBacklogBCL.js
+++ frontend/src/Components/BCL/IssueBacklog/IssueBacklogBCL.js
@@ -1,81 +1,169 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import SetPagination from '../../Common/Pagination/Pagination';
+import { Table, Row, Col, Button, Card, NavLink, Form, FormControl, Modal, Badge } from 'react-bootstrap';
+import IssueForm from '../CreateIssue/IssueForm'
+import Issuecard from '../IssueTable/Issecard'
+import './table.css';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './IssueBacklogBCL.scss'
 
+//my
+function bagetype(priority) {
+
+  switch (priority) {
+    case 'High':
+      return 'danger';
+    case 'Medium':
+      return 'warning';
+    case 'Low':
+      return 'success'
+  }
+}
 
 function IssueBacklogBCL() {
 
-  const bugs = ["bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1","bug1"]
+  const [isModelOpen, setisModelOpen] = useState(false);
+  const [buglist, setbuglist] = useState([])
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [IssuePerPage, setIssuePerPage] = useState(7);
-  const indexOfLastIssue = currentPage * IssuePerPage;
-  const indexOfFirstIssue = indexOfLastIssue - IssuePerPage;
-  const currentIssue = bugs.slice(indexOfFirstIssue, indexOfLastIssue);
+  function tableticket(e) {
+    for (var i = 0; i < buglist.length; i++) {
+      if (buglist[i].id === e) {
+        render(<Issuecard priority={buglist[i].priority} type={buglist[i].type} summary={buglist[i].summary} variant={bagetype(buglist[i].priority)} />);
+      }
+    }
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  }
 
   return (
-    <div class="d-flex fontSize" id="wrapper">
-      <div id="page-content-wrapper">
+    <div className="">
 
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Dashboard</h1>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
-              <button class="btn btn-sm btn-outline-secondary">Add Issue</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar"></span>
-              Sort by
-            </button>
-          </div>
-        </div>
+      <Row >
+        {/* <div className="col-md-2 bg-dark">
+          <Col className=" text-white  ">
 
-        <div class="card ml-3 mt-2 mb-3 mr-3">
+            <Row> <NavLink className="d-inline p-2 bg-dark text-white" to="/Projectlist"> LOGO</NavLink>
+            </Row>
+            <Row> <NavLink className="d-inline p-2 bg-dark text-white" to="/Projectlist">  Summary</NavLink>
+            </Row>
+            <Row> <NavLink className="d-inline p-2 bg-dark text-white" to="/Projectlist"> Dashboard</NavLink>
+            </Row>
+            <Row> <NavLink className="d-inline p-2 bg-dark text-white" to="/Projectlist"> Reports </NavLink>
+            </Row>
 
-          <div class="card-header text-uppercase">
-            <h6 class="d-inline">Project A</h6>
-            <a href="" class="float-right text-dark"><i class="fa fa-search"></i></a>
-          </div>
 
-          <div class="list-group mb-0">
 
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">BugName</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentIssue.map(bug=>(
+          </Col>
+
+        </div> */}
+        <div className="ml-2 mt-2 col-md-9">
+          <Col className="">
+            <Row className="">
+              <Card>
+                <Card.Body>
+                  <Card.Title>Project 1</Card.Title>
+                  <Card.Text>
+                    Description <br></br>
+                        Project Name, ID,
+                        Customer ID
+                     </Card.Text>
+
+                </Card.Body>
+              </Card>
+            </Row>
+            <Row className="border border-dark mt-2 mb-2" >
+              <div className="d-flex p-2 bd-highlight">
+                <Col md={4}>
+
+
+                  <Button className="mr-sm-2" variant="info" data-toggle="tooltip" title="Go to issues"
+                    onClick={() => setisModelOpen(true)}
+                  >
+                    Add Issue</Button>
+                  <Modal size="lg" show={isModelOpen}>
+                    <Modal.Body>
+                      <IssueForm cl={() => setisModelOpen(false)} buglist={buglist} setbuglist={setbuglist} />
+
+                    </Modal.Body>
+
+                  </Modal>
+
+                </Col>
+                <Col md={3}>
+                  {/* sort here */}
+
+                </Col>
+                <Col md={5}>
+                  <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" /> {/* Margin right(Padding , Small and up by 2) */}
+                  </Form>
+                </Col>
+              </div>
+            </Row>
+            {/* <Row>
+
+                          {
+                              bug.map((bug) => <Issuecard id={'#' + bug.id} summ={bug.Summary} priority={bug.priority} badgetype={bagetype(bug.priority)} />)
+                          }
+
+
+                      </Row> */}
+            <Row>
+              <Table>
+                <thead>
                   <tr>
-                    <th scope="row">{bugs.indexOf(bug)}</th>
-                    <td>{bug}</td>
+                    <th>
+                      #id
+                    </th>
+                    <th>
+                      Title
+                    </th>
+                    <th>
+                      Priority
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {
+                    buglist.map((bug) =>
+                      <tr className="highligh" id={bug.id} onClick={() => { tableticket(bug.id) }} key={bug.id}>
+                        <td>{'#' + bug.id}</td>
+                        <td className="noover"><div>{bug.title}</div></td>
+                        <td><Badge variant={bagetype(bug.priority)}>{bug.priority}</Badge></td>
+                      </tr>
+                    )
+                  }
 
-          </div>
+                </tbody>
+              </Table>
+            </Row>
 
+
+
+            <Row >
+
+              <div>
+                <Card className=" mu-2">
+                  <Card.Body>
+
+                    {/* <SetPagination
+                      IssuePerPage={IssuePerPage}
+                      totalIssues={bugs.length}
+                      paginate={paginate}
+                    /> */}
+
+                  </Card.Body>
+                </Card>
+              </div>
+            </Row>
+          </Col>
         </div>
-
-      <div class="row ml-3 mt-2 mb-3 mr-3">
-        <SetPagination
-          IssuePerPage={IssuePerPage}
-          totalIssues={bugs.length}
-          paginate={paginate}
-        />
-      </div>
-
-      </div>
-
-      
+      </Row>
 
     </div>
   )
+
+
 }
 
 
